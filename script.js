d8a04764dc5123c556fe0d429a0190ce8bb41b77
J <dpsdamagepersecond@outlook.com>
2022-12-04 15:32:39 -0800

    UI added kind of

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //score
 let playerScore = 0;
 let computerScore = 0;
-
 //score
+
+const result = document.querySelector(".result");
+
+let playerPick = "";
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -28,48 +31,78 @@ function playerChoice() {
 function round(player, computer) {
     if (player === computer) {
         console.log("TIE")
+        result.textContent = "It's a tie!"
         return("TIE");
     } else if (player === "ROCK" && computer === "PAPER") {
         console.log("LOSS")
         computerScore++;
+        result.textContent = "It's a loss!";
         return("LOSS");
     } else if (player === "ROCK" && computer === "SCISSORS") {
         console.log("WIN")
         playerScore++;
+        result.textContent = "It's a win!"
         return("WIN");
-    } else if (player === "PAPER" && computer === "ROCK") {
-        console.log("LOSS")
-        computerScore++;
-        return("LOSS");
     } else if (player === "PAPER" && computer === "SCISSORS") {
-        console.log("WIN")
-        playerScore++;
-        return("WIN");
-    } else if (player === "SCISSORS" && computer === "PAPER") {
         console.log("LOSS")
         computerScore++;
+        result.textContent = "It's a loss!"
         return("LOSS");
-    } else if (player === "SCISSORS" && computer === "ROCK") {
+    } else if (player === "PAPER" && computer === "ROCK") {
         console.log("WIN")
         playerScore++;
+        result.textContent = "It's a win!"
+        return("WIN");
+    } else if (player === "SCISSORS" && computer === "ROCK") {
+        console.log("LOSS")
+        computerScore++;
+        result.textContent = "It's a loss!"
+        return("LOSS");
+    } else if (player === "SCISSORS" && computer === "PAPER") {
+        console.log("WIN")
+        playerScore++;
+        result.textContent = "It's a win!"
         return("WIN");
     }
 }
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
+// function game() {
+//     playerScore = 0;
+//     computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        round(playerChoice(), getComputerChoice());
-    }
-    if (computerScore === playerScore) {
-        console.log("THE WHOLE GAME WAS A DRAW")
-    } else if (playerScore > computerScore) {
-        console.log("EASY WIN TOO EASY")
-    } else {
-        console.log("YOU LOST IMAGINE HAHAHA")
-    }
-}
+//     for (let i = 0; i < 5; i++) {
+//         round(playerChoice(), getComputerChoice());
+//     }
+//     if (computerScore === playerScore) {
+//         console.log("THE WHOLE GAME WAS A DRAW")
+//     } else if (playerScore > computerScore) {
+//         console.log("EASY WIN TOO EASY")
+//     } else {
+//         console.log("YOU LOST IMAGINE HAHAHA")
+//     }
+// }
 
-game()
+// game()
+
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+rock.addEventListener("click", () => {
+    playerPick = "ROCK";
+    console.log("ROCK");
+    round(playerPick, getComputerChoice());
+});
+
+paper.addEventListener("click", () => {
+    playerPick = "PAPER";
+    console.log("PAPER");
+    round(playerPick, getComputerChoice());
+});
+
+scissors.addEventListener("click", () => {
+    playerPick = "SCISSORS";
+    console.log("SCISSORS");
+    round(playerPick, getComputerChoice());
+});
